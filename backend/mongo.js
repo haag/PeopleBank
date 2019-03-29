9261847eafdbf08944cdf2e4522a44ffceba87b0
Haag <codehaag@gmail.com>
2019-03-29 15:51:27 -0600

    small changes

--- backend/mongo.js
+++ backend/mongo.js
@@ -4,14 +4,17 @@ mongoose.Promise = global.Promise
 
 function connect() {
     mongoose.set('debug', true);
-    return mongoose.connect(process.env.COSMOSDB_CONNSTR+"?ssl=true&replicaSet=globaldb", {
-            auth: {
+    return mongoose.connect(process.env.COSMOSDB_CONNSTR+"?ssl=true&replicaSet=globaldb", 
+    { 
+        useCreateIndex: true, 
+        useNewUrlParser: true, 
+        auth: {
               user: process.env.COSMODDB_USER,
               password: process.env.COSMOSDB_PASSWORD
-            }
-          })
-          .then(() => console.log('Connection to CosmosDB successful'))
-          .catch((err) => console.error(err));
+            }        
+    })
+        .then(() => console.log('Connection to CosmosDB successful'))
+        .catch((err) => console.error(err));
 }
 
 
--- backend/todo.md
+++ backend/todo.md
@@ -1,4 +1,4 @@
-[] Get connected to new Windows env
-[] Connect to CosmosDB
-[] Add git aliases
+[x] Get connected to new Windows env
+[x] Connect to CosmosDB
+[x] Add git aliases
 [] 
